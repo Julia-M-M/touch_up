faeab15246a7e0b64317d6dc645ec22d9132114a
Julia Moreno-Manjón <84106177+Julia-M-M@users.noreply.github.com>
2022-05-11 16:33:56 +0100

    fixed description

--- del_lines.py
+++ del_lines.py
@@ -1,7 +1,12 @@
-words = ['OXA-51']
+##DELETE LINES IN TEXT THAT CONTAIN OR DO NOT CONTAIN CERTAIN CHARACTERS ##
+
+# Specify the words
+words = ['write', 'characters', 'here']
 
 with open('input.txt') as input, open('output.txt', 'w') as output:
     for line in input:
+        # Delete line if it does not contain any of the words
         if any(word in line for word in words):
+        # Delete line if it contains any of the words
         #if not any(word in line for word in words):
             output.write(line)
